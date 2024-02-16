2446e5a95a12496e28d43154d44d37666ee07970
Vitalya <vitalyabes.vb@gmail.com>
2024-02-16 09:49:17 +0300

    Initial commit

---
+++ unique_ptr do it yourself.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+template<class T>
+class unique_ptr {
+private:
+    T* ptr_;
+public:
+
+    unique_ptr(T* ptr): ptr_(ptr) {
+        
+    }
+
+    unique_ptr(T other) = delete;
+
+    T operator= (T other) = delete;
+
+    int operator * ( ) {
+        return ptr_[0];
+
+    }
+
+    void operator << (unique_ptr other) {
+        std::cout << other;
+    }
+
+    unique_ptr release() {
+        ptr_ = nullptr;
+        return ptr_;
+
+    }
+
+    ~unique_ptr() {
+        delete ptr_;
+    }
+
+
+};
+
+
+int main()
+{
+    int* ptr = new int[2] {4,6};
+    unique_ptr<int> qwe(ptr);
+    
+    std::cout << "\n";
+    std::cout << *qwe;
+    qwe.release();
+
+    return 0;
+}
